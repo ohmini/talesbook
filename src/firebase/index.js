c4d9ef116327c23ac161a625f583759c3a6f0f63
Ratthasorn Thassana <ratthasorn.thassana@gmail.com>
2018-01-20 11:04:09 +0700

    fix firebase config

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 
 var config = {
-  apiKey: "AIzaSyCY8ViJ9E_F5d2e-z1uExe5gVX8YYCs35g",
-  authDomain: "swine-sc.firebaseapp.com",
-  databaseURL: "https://swine-sc.firebaseio.com",
-  projectId: "swine-sc",
-  storageBucket: "swine-sc.appspot.com",
-  messagingSenderId: "16743347846"
+    apiKey: "AIzaSyCY8ViJ9E_F5d2e-z1uExe5gVX8YYCs35g",
+    authDomain: "talsebook-5b1ea.firebaseapp.com",
+    databaseURL: "https://talsebook-5b1ea.firebaseio.com",
+    projectId: "talsebook-5b1ea",
+    storageBucket: "talsebook-5b1ea.appspot.com",
+    messagingSenderId: "902248397103"
 }
 firebase.initializeApp(config)
 
