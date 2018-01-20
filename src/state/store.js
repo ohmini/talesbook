fc217073c629e5b074b78db4c055afb9c9834ece
Ratthasorn Thassana <ratthasorn.thassana@gmail.com>
2018-01-20 11:47:51 +0700

    add saga config

--- src/state/store.js
+++ src/state/store.js
@@ -1,19 +1,26 @@
 import {createStore, applyMiddleware, combineReducers, compose} from 'redux'
 import {router5Middleware as createRouterMiddleware, router5Reducer} from 'redux-router5'
+import createSagaMiddleware from 'redux-saga'
+
+import {effects, reducer} from './ducks'
 import { router } from '../router';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const routerMiddleware = createRouterMiddleware(router)
+const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(
     combineReducers({
         router: router5Reducer,
+        ...reducer
     }),
     {},
     composeEnhancers(applyMiddleware(
-        routerMiddleware
+        routerMiddleware, sagaMiddleware
     ))
 )
+
+effects.forEach(fx => sagaMiddleware.run(fx))
 
 export {
     store
